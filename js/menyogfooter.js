59a0324f7c1494b2daef58f775544beac9b76515
Benni <bben.solli@gmail.com>
2019-05-23 20:07:27 +0200

    Initial commit

---
+++ js/menyogfooter.js
@@ -0,0 +1,42 @@
+
+
+var menuOpen = false;
+
+var timeoutNumber;
+document.getElementById("burgermenu").addEventListener("click", function() { 
+    let body = document.getElementById("body");
+    clearTimeout(timeoutNumber);
+    if (menuOpen) {
+        menuOpen = false;
+        body.classList.remove('menuOpen');
+        timeoutNumber = setTimeout(function() {
+            body.classList.remove('menuOpening');
+        }, 500);
+    } else {
+        menuOpen = true;
+        body.classList.add('menuOpening');
+        timeoutNumber = setTimeout(function() {
+            body.classList.add('menuOpen');
+        }, 0);
+    } 
+});
+
+document.getElementById("burgermenu__x").addEventListener("click", function() { 
+    let body = document.getElementById("body");
+    clearTimeout(timeoutNumber);
+    if (menuOpen) {
+        menuOpen = false;
+        body.classList.remove('menuOpen');
+        timeoutNumber = setTimeout(function() {
+            body.classList.remove('menuOpening');
+        }, 500);
+    } else {
+        menuOpen = true;
+        body.classList.add('menuOpening');
+        timeoutNumber = setTimeout(function() {
+            body.classList.add('menuOpen');
+        }, 0);
+    } 
+});
+
+
