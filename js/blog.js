ae628386210f85efaa08b768332630dd483a39db
Benni <bben.solli@gmail.com>
2019-06-02 22:23:24 +0200

    nit picking

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,22 @@
 
 /*blog nyheter*/
 
+function hoverEffect(id)  {
+    var x = document.getElementById("nyhetH" + id);
+    var y = document.getElementById("nyhetImg" + id);
+    x.classList.add('blog__text_style');
+    y.classList.add('blog__img_style');
+}
 
+function hoverOff(id) {
+    var x = document.getElementById("nyhetH" + id);
+    var y = document.getElementById("nyhetImg" + id);
+    x.classList.remove('blog__text_style');
+    y.classList.remove('blog__img_style');    
+}
+
+
+/*
 function hoverOff(id) {
     var x = document.getElementById("nyhetH" + id);
     var y = document.getElementById("nyhetImg" + id);
@@ -34,4 +49,6 @@ function hoverEffect(id)  {
         x.style.textDecoration = "underline";
       
     }
-}
+}*/
+
+
